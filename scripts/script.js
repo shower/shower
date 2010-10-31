2c776daaec80efab30e0a8876dd7b996d5e58158
myfreeweb <me@myfreeweb.ru>
2010-10-31 19:53:04 +0300

    instead of jQuery, not fullscreen-only, lowercase CSS, etc.

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,30 @@
-$(function() {
-
-	var slides = $( 'section.slide' );
-
-	function fullScreen() {
-		return (
-			window.screenX == 0 &&
-			window.screenY == 0 &&
-			screen.width == window.outerWidth &&
-			screen.height == window.outerHeight
-		) ? true : false;
-	}
+$dom.onready(function() {
+	var slides = $dom.get('.slide');
 	
-	function turnSlide( e ) {
-		if( !fullScreen() ) return;
-		var current = $( document.location.hash );
-		switch ( e.which ) {
+	function turnSlide(e) {
+		var current = $dom.get(document.location.hash)[0];
+		switch (e.which) {
 			case 33 : // PgUp
 			case 38 : // Up
 			case 37 : // Left
-				var target = current.prev( slides );
+			var target = $dom.previous(current, slides);
 				break;
 			case 34 : // PgDown
 			case 40 : // Down
 			case 39 : // Right
-				var target = current.next( slides );
+			var target = $dom.next(current, slides);
 				break;
 			case 32 : // Space
-				var target = current[ e.shiftKey ? 'prev' : 'next' ]( slides );
+			var target = $dom[e.shiftKey ? 'previous' : 'next'](current, slides);
 				break;
 			default:
 				return;
 		}
 		e.preventDefault();
-		if( !target.length ) return;
-		else document.location.hash = '#' + target.attr( 'id' );
+		if(typeof(target) == 'undefined') return;
+		else document.location.hash = '#' + target.id;
 	}
 
-	function updateView() {
-		var screen = $( 'link[title=screen]' ),
-			projection = $( 'link[title=projection]' ),
-			fullscreen = fullScreen();
-		screen.attr( 'disabled', fullscreen );
-		projection.attr( 'disabled', !fullscreen );
-		if( !fullscreen || $( document.location.hash ).length ) return;
-		else document.location.hash = '#' + slides.first().attr( 'id' );
-	}
-
-	$( document ).
-		keyup( turnSlide );
-
-	$( window ).
-		ready( updateView ).
-		resize( updateView );
-
+	document.onkeyup = turnSlide;
+    document.location.hash = '#' + slides[0].id;
 });
