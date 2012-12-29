e9a9c353cf8731aa7be2813f8417053ba1e51a71
Frederic Hemberger <mail@frederic-hemberger.de>
2012-12-29 14:58:41 +0100

    Check if Shower is already in slide/list mode

--- shower.js
+++ shower.js
@@ -131,7 +131,9 @@ window.shower = (function(window, document, undefined) {
 	* @returns {number}
 	*/
 	shower.enterSlideMode = function() {
-		// @TODO: check if it's already in slide mode...
+		// check if it's already in slide mode...
+		if ( body.classList.contains('full') ) { return; }
+		
 		body.classList.remove('list');
 		body.classList.add('full');
 		return shower._applyTransform(shower._getTransform());
@@ -142,7 +144,9 @@ window.shower = (function(window, document, undefined) {
 	* @returns {number}
 	*/
 	shower.enterListMode = function() {
-		// @TODO: check if it's already in list mode...
+		// check if it's already in list mode...
+		if ( body.classList.contains('list') ) { return; }
+		
 		body.classList.remove('full');
 		body.classList.add('list');
 		return shower._applyTransform('none');
