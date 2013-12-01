1e2e94184dff370ad2b90a27ba471f51860d9b8d
Vadim Makeev <pepelsbey@gmail.com>
2013-12-01 10:08:39 +0400

    New build and test system

---
+++ packages/ribbon/Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			compile: {
+				files: {
+					'styles/screen.css': 'styles/screen.scss'
+				}
+			}
+		},
+		autoprefixer: {
+			prefix: {
+				src: 'styles/screen.css'
+			}
+		},
+		csso: {
+			minify: {
+				files: {
+					'styles/screen.css' : 'styles/screen.css'
+				},
+				options: {
+					banner: '/**\n * Ribbon theme for Shower HTML presentation engine: github.com/shower/shower\n * Copyright © 2010–<%= grunt.template.today("yyyy") %> Vadim Makeev, pepelsbey.net\n * Licensed under MIT license: github.com/shower/shower/wiki/MIT-License\n */\n'
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-csso');
+
+	grunt.registerTask('default', ['sass', 'autoprefixer', 'csso']);
+
+};
