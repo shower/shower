28f9dc4e6807cda832a32e7cdc5cdb19d8f1fd64
Vadim Makeev <pepelsbey@gmail.com>
2013-12-06 10:08:50 +0100

    Watcher for grunt

--- packages/ribbon/Gruntfile.js
+++ packages/ribbon/Gruntfile.js
@@ -23,12 +23,19 @@ module.exports = function(grunt) {
 					banner: '/**\n * Ribbon theme for Shower HTML presentation engine: github.com/shower/shower\n * Copyright © 2010–<%= grunt.template.today("yyyy") %> Vadim Makeev, pepelsbey.net\n * Licensed under MIT license: github.com/shower/shower/wiki/MIT-License\n */\n'
 				}
 			}
+		},
+		watch: {
+			styles: {
+				files: 'styles/*.scss',
+				tasks: ['sass', 'autoprefixer', 'csso']
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-csso');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('default', ['sass', 'autoprefixer', 'csso']);
 
