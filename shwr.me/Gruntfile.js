44676f5b66faae323c1a421ba2c6e37a974cfc67
Vadim Makeev <pepelsbey@gmail.com>
2013-12-10 01:20:32 +0400

    Even better compress task

--- shwr.me/Gruntfile.js
+++ shwr.me/Gruntfile.js
@@ -1,41 +1,33 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
-		compress: (function() {
-			var modules = [
-				'template',
-				'shower',
-				'ribbon',
-				'bright'
-			];
-
-			var excludes = [
-				'**',
-				'!node_modules/**',
-				'!**/Contributing.md',
-				'!**/Gruntfile.js',
-				'!**/package.json',
-				'!**/index.pdf',
-				'!**/tests/**'
-			];
-
-			var result = {}
-
-			for (var i = 0, l = modules.length; i < l; i++) {
-				result[modules[i]] = {
-					options: {
-						archive: modules[i] + '.zip'
-					},
-					files: [{
-						expand: true,
-						cwd: modules[i] + '/',
-						src: excludes
-					}]
-				};
-			}
+		compress: [
+			'template',
+			'shower',
+			'ribbon',
+			'bright'
+		].reduce(function(result, module) {
+			result[module] = {
+				options: {
+					archive: module + '.zip'
+				},
+				files: [{
+					expand: true,
+					cwd: module + '/',
+					src: [
+						'**',
+						'!node_modules/**',
+						'!**/Contributing.md',
+						'!**/Gruntfile.js',
+						'!**/package.json',
+						'!**/index.pdf',
+						'!**/tests/**'
+					]
+				}]
+			};
 
 			return result;
-		})()
+		}, {})
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-compress');
