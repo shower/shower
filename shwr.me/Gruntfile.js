ff96c55d27fbf805b735e3a3a4e63a1f3299cb30
Vadim Makeev <pepelsbey@gmail.com>
2013-12-13 04:05:38 +0400

    New deployment is ready

--- shwr.me/Gruntfile.js
+++ shwr.me/Gruntfile.js
@@ -1,6 +1,14 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
+		copy: {
+			duplicate: {
+				expand: true,
+				cwd: 'template/',
+				src: '**',
+				dest: 'temporary/'
+			}
+		},
 		compress: [
 			'template',
 			'shower',
@@ -9,13 +17,11 @@ module.exports = function(grunt) {
 		].reduce(function(result, module) {
 			result[module] = {
 				options: {
-					archive: module + '.zip'
+					archive: 'temporary/' + module + '.zip'
 				},
 				files: [{
-					expand: true,
-					cwd: module + '/',
 					src: [
-						'**',
+						module + '/**',
 						'!node_modules/**',
 						'!**/Contributing.md',
 						'!**/Gruntfile.js',
@@ -27,10 +33,41 @@ module.exports = function(grunt) {
 			};
 
 			return result;
-		}, {})
+		}, {}),
+		replace: {
+			counter: {
+				src: 'temporary/index.html',
+				overwrite: true,
+				replacements: [{
+					from: '</body>',
+					to: '\t<%= grunt.file.read("counter.html") %>\n</body>'
+				}]
+			}
+		},
+		exec: {
+			deploy: {
+				cmd: 'rsync -rz\
+					--exclude ".DS_Store"\
+					--exclude ".git*"\
+					--exclude "Contributing.md"\
+					--exclude "License.md"\
+					--exclude "Readme.md"\
+					--exclude "node_modules"\
+					--exclude "tests"\
+					--exclude "Gruntfile.js"\
+					--exclude "package.json"\
+					temporary/ pepelsbey@shwr.me:shwr.me/temp/'
+			}
+		},
+		clean: ['temporary']
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-compress');
-	grunt.registerTask('default', ['compress']);
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-text-replace');
+	grunt.loadNpmTasks('grunt-exec');
+
+	grunt.registerTask('default', ['copy', 'compress', 'replace', 'exec', 'clean']);
 
 };
