cca3eab3a37a30c4419dbaeeaa7fd52955d418c7
Vadim Makeev <pepelsbey@gmail.com>
2015-12-05 20:28:40 +0100

    Trailing slashes removed, BS ui turned off

--- packages/material/gulpfile.js
+++ packages/material/gulpfile.js
@@ -24,10 +24,11 @@ var banner = `/**
 
 gulp.task('default', ['styles'], function() {
 	sync.init({
+		ui: false,
 		notify: false,
 		server: {
-			baseDir: './',
-			routes: {
+			baseDir: '.',
+ 			routes: {
 				'/shower-core': '../shower-core'
 			}
 		}
@@ -45,6 +46,6 @@ gulp.task('styles', function () {
 		.pipe(autoprefixer())
 		.pipe(minify())
 		.pipe(header(banner, { pkg: pkg }))
-		.pipe(gulp.dest('styles/'))
+		.pipe(gulp.dest('styles'))
 		.pipe(sync.stream());
 });
