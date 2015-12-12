9665ef9cc3e19af886d64bc1969e8b4253565a37
Vadim Makeev <pepelsbey@gmail.com>
2015-12-13 00:40:12 +0300

    Merge branch 'next' of github.com:shower/ribbon
    * 'next' of github.com:shower/ribbon: (194 commits)
      Updated readme
      Docs added to packages
      Trailing slashes removed, BS ui turned off
      Updated dependencies; npm scripts
      Build
      New readme
      Placeholders for docs
      Build
      Shorter columns sample
      Block for <main>, just in case
      Docs fix
      Full-width tables
      Highlighted code rows
      Updated dependencies
      Updated dependencies
      Clean up
      File modes
      Pixels are back!
      Better clean-css wrapper: gulp-minify-css
      Updatede dependencies
      ...
    
    # Conflicts:
    #	bower.json
    #	index.html
    #	package.json
    #	styles/defaults.scss
    #	styles/fonts.scss
    #	styles/screen.css
    #	styles/screen.scss

---
+++ packages/ribbon/gulpfile.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var autoprefixer = require('gulp-autoprefixer'),
+	minify = require('gulp-minify-css'),
+	gulp = require('gulp'),
+	header = require('gulp-header'),
+	sass = require('gulp-sass'),
+	sync = require('browser-sync').create();
+
+require('gulp-release-it')(gulp);
+
+// Banner
+
+var pkg = require('./package.json');
+var banner = `/**
+ * ${ pkg.description }
+ * ${ pkg.name } v${ pkg.version }, ${ pkg.homepage }
+ * @copyright 2010–${ new Date().getFullYear() } ${ pkg.author.name }, ${ pkg.author.url }
+ * @license ${ pkg.license }
+ */
+`;
+
+// Server
+
+gulp.task('default', ['styles'], function() {
+	sync.init({
+		ui: false,
+		notify: false,
+		server: {
+			baseDir: '.',
+			routes: {
+				'/shower-core': '../shower-core'
+			}
+		}
+	});
+
+	gulp.watch('styles/**/*.scss', ['styles']);
+	gulp.watch('index.html').on('change', sync.reload);
+});
+
+// Styles
+
+gulp.task('styles', function () {
+	return gulp.src('styles/screen-*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(minify())
+		.pipe(header(banner, { pkg: pkg }))
+		.pipe(gulp.dest('styles'))
+		.pipe(sync.stream());
+});
