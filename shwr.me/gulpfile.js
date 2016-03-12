3703357cd46fc5225fc3d31a4fc97696e409afef
Vadim Makeev <pepelsbey@gmail.com>
2016-03-12 15:58:18 +0300

    Fully-functioning template

--- shwr.me/gulpfile.js
+++ shwr.me/gulpfile.js
@@ -5,24 +5,52 @@ var gulp = require('gulp'),
 
 gulp.task('default', function() {
 
-	var shower = gulp.src('node_modules/shower/**');
+	var shower = gulp.src([
+			'node_modules/shower/**', '!package.json'
+		])
+		.pipe(replace(
+			/(<link rel="stylesheet" href=")(node_modules\/shower-ribbon\/)(styles\/screen-16x10.css">)/g,
+			'$1shower/themes/ribbon/$3', { skipBinary: true }
+		))
+		.pipe(replace(
+			/(<script src=")(node_modules\/shower-core\/)(shower.min.js"><\/script>)/g,
+			'$1shower/$3', { skipBinary: true }
+		));
 
-	var core = gulp.src('node_modules/shower-core/shower.min.js', { base: 'node_modules/shower-core' })
+	var core = gulp.src([
+			'shower.min.js'
+		], {
+			cwd: 'node_modules/shower-core'
+		})
 		.pipe(rename(function (path) {
 			path.dirname = 'shower/' + path.dirname;
 		}));
 
-	var material = gulp.src('node_modules/shower-material/**')
+	var material = gulp.src([
+			'**', '!package.json'
+		], {
+			cwd: 'node_modules/shower-material'
+		})
 		.pipe(rename(function (path) {
 			path.dirname = 'shower/themes/material/' + path.dirname;
-		}));
+		}))
 
-	var ribbon = gulp.src('node_modules/shower-ribbon/**')
+	var ribbon = gulp.src([
+			'**', '!package.json'
+		], {
+			cwd: 'node_modules/shower-ribbon'
+		})
 		.pipe(rename(function (path) {
 			path.dirname = 'shower/themes/ribbon/' + path.dirname;
 		}));
 
-	return merge(shower, core, material, ribbon)
+	var themes = merge(material, ribbon)
+		.pipe(replace(
+			/(<script src=")(\/shower-core\/)(shower.min.js"><\/script>)/,
+			'$1../../$3', { skipBinary: true }
+		));
+
+	return merge(shower, core, themes)
 		.pipe(gulp.dest('dest'));
 
 });
