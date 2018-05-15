fab5c7e2c65c57d0acb3e17a477c8682544b7a25
Nikolay Govorov <nikolay_govorov@bk.ru>
2018-05-15 23:15:12 +0300

    Create basic structure

--- packages/cli/bin/shower.js
+++ packages/cli/bin/shower.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-console.log('Shower CLI');
+const command = process.argv[2];
+
+const state = {
+  root: process.env.PWD,
+};
+
+if (command !== undefined) {
+  try {
+    const task = require(`../lib/${command}`);
+
+    task(state);
+  } catch (_) {
+    console.error(`Unknow task "${command}"`);
+  }
+} else {
+  console.error('Required task name');
+}
