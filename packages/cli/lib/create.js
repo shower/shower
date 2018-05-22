0522a46c7aaf9c4620f8d27b0a7a4910c139cc6c
Nikolay Govorov <nikolay_govorov@bk.ru>
2018-05-23 00:37:15 +0300

    Removed custom download

--- packages/cli/lib/create.js
+++ packages/cli/lib/create.js
@@ -1,25 +1,7 @@
-const path = require('path')
-
-const unzip = require('./utils/unzip')
-const remove = require('./utils/remove')
-const download = require('./utils/download')
+const download = require('download')
 
 module.exports = function create ({ root }, { url }) {
   console.log('Run to create new project\n')
 
-  const archive = path.join(root, 'shower.zip')
-
-  return Promise.resolve()
-    .then(() => {
-      console.log(`-- Download template...`)
-      return download({ url, destination: archive })
-    })
-    .then(() => {
-      console.log(`-- Unzip...`)
-      return unzip({ file: archive, destination: root })
-    })
-    .then(() => {
-      console.log(`-- Clear...`)
-      return remove({ file: archive })
-    })
+  return download(url, root, { extract: true })
 }
--- packages/cli/lib/utils/remove.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-const { promisify } = require('util')
-
-module.exports = function remove ({ file }) {
-  return promisify(fs.unlink)(file)
-}
