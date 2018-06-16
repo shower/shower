8ff15284bbfcc825b8db07a853279ff256524488
Nikolay Govorov <nikolay_govorov@bk.ru>
2018-06-16 20:07:14 +0300

    Testing JS API

--- packages/cli/test/index.test.js
+++ packages/cli/test/index.test.js
@@ -1,1 +1,21 @@
-it('fake test', () => {})
+const index = require('../index')
+
+it('Must be a object', () => {
+  expect(typeof index === 'object' && index !== null).toBe(true)
+})
+
+it('Must contain libs', () => {
+  const keys = Object.keys(index)
+
+  expect(keys).toEqual(['create', 'pdf', 'serve'])
+})
+
+it('Libs must be functions', () => {
+  for (const lib in index) {
+    if (!index.hasOwnProperty(lib)) {
+      continue
+    }
+
+    expect(typeof index[lib]).toBe('function')
+  }
+})
