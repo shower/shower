585a46e4dec98eb78bf68129ae364c386b465e9b
Vadim Makeev <pepelsbey@gmail.com>
2018-06-27 01:01:11 +0300

    Change code style from tabs to spaces

--- shwr.me/gulpfile.js
+++ shwr.me/gulpfile.js
@@ -8,107 +8,107 @@ const rsync = require('gulp-rsync');
 const zip = require('gulp-zip');
 
 gulp.task('clean', () => {
-	return del('dest/**');
+    return del('dest/**');
 });
 
 gulp.task('build', () => {
-	const shower = gulp.src([
-			'**',
-			'!package.json'
-		], {
-			cwd: 'node_modules/shower'
-		})
-		.pipe(replace(
-			/(<link rel="stylesheet" href=")(node_modules\/shower-ribbon\/)(styles\/styles.css">)/g,
-			'$1shower/themes/ribbon/$3', { skipBinary: true }
-		))
-		.pipe(replace(
-			/(<script src=")(node_modules\/shower-core\/)(shower.min.js"><\/script>)/g,
-			'$1shower/$3', { skipBinary: true }
-		));
+    const shower = gulp.src([
+            '**',
+            '!package.json'
+        ], {
+            cwd: 'node_modules/shower'
+        })
+        .pipe(replace(
+            /(<link rel="stylesheet" href=")(node_modules\/shower-ribbon\/)(styles\/styles.css">)/g,
+            '$1shower/themes/ribbon/$3', { skipBinary: true }
+        ))
+        .pipe(replace(
+            /(<script src=")(node_modules\/shower-core\/)(shower.min.js"><\/script>)/g,
+            '$1shower/$3', { skipBinary: true }
+        ));
 
-	const core = gulp.src([
-			'shower.min.js'
-		], {
-			cwd: 'node_modules/shower-core'
-		})
-		.pipe(rename( (path) => {
-			path.dirname = 'shower/' + path.dirname;
-		}));
+    const core = gulp.src([
+            'shower.min.js'
+        ], {
+            cwd: 'node_modules/shower-core'
+        })
+        .pipe(rename( (path) => {
+            path.dirname = 'shower/' + path.dirname;
+        }));
 
-	const material = gulp.src([
-			'**',
-			'!package.json'
-		], {
-			cwd: 'node_modules/shower-material'
-		})
-		.pipe(rename( (path) => {
-			path.dirname = 'shower/themes/material/' + path.dirname;
-		}))
+    const material = gulp.src([
+            '**',
+            '!package.json'
+        ], {
+            cwd: 'node_modules/shower-material'
+        })
+        .pipe(rename( (path) => {
+            path.dirname = 'shower/themes/material/' + path.dirname;
+        }))
 
-	const ribbon = gulp.src([
-			'**',
-			'!package.json'
-		], {
-			cwd: 'node_modules/shower-ribbon'
-		})
-		.pipe(rename( (path) => {
-			path.dirname = 'shower/themes/ribbon/' + path.dirname;
-		}));
+    const ribbon = gulp.src([
+            '**',
+            '!package.json'
+        ], {
+            cwd: 'node_modules/shower-ribbon'
+        })
+        .pipe(rename( (path) => {
+            path.dirname = 'shower/themes/ribbon/' + path.dirname;
+        }));
 
-	const themes = merge(material, ribbon)
-		.pipe(replace(
-			/(<script src=")(node_modules\/shower-core\/)(shower.min.js"><\/script>)/g,
-			'$1../../$3', { skipBinary: true }
-		));
+    const themes = merge(material, ribbon)
+        .pipe(replace(
+            /(<script src=")(node_modules\/shower-core\/)(shower.min.js"><\/script>)/g,
+            '$1../../$3', { skipBinary: true }
+        ));
 
-	return merge(shower, core, themes)
-		.pipe(gulp.dest('dest'))
-		.pipe(zip('shower.zip'))
-		.pipe(gulp.dest('dest'));
+    return merge(shower, core, themes)
+        .pipe(gulp.dest('dest'))
+        .pipe(zip('shower.zip'))
+        .pipe(gulp.dest('dest'));
 });
 
 gulp.task('assets', () => {
-	const files = gulp.src([
-		'icons{,/**}',
-		'.webmanifest',
-		'favicon.ico'
-	]);
+    const files = gulp.src([
+        'icons{,/**}',
+        '.webmanifest',
+        'favicon.ico'
+    ]);
 
-	const html = gulp.src('dest/**/*.html')
-		.pipe(replace(
-			/(<meta name="viewport" content="width=device-width, initial-scale=1">)/,
-			`$1
-	<link rel="manifest" href="/.webmanifest">
-	<link rel="icon" href="/favicon.ico">
-	<link rel="icon" type="image/png" sizes="228x228" href="/icons/228.png">
-	<link rel="apple-touch-icon" type="image/png" href="/icons/228.png">`, { skipBinary: true }
-		))
-		.pipe(replace(
-			/(<\/body>)/,
-			`\t${fs.readFileSync('counter.html', 'utf8')}$1`, { skipBinary: true }
-		));
+    const html = gulp.src('dest/**/*.html')
+        .pipe(replace(
+            /(<meta name="viewport" content="width=device-width, initial-scale=1">)/,
+            `$1
+    <link rel="manifest" href="/.webmanifest">
+    <link rel="icon" href="/favicon.ico">
+    <link rel="icon" type="image/png" sizes="228x228" href="/icons/228.png">
+    <link rel="apple-touch-icon" type="image/png" href="/icons/228.png">`, { skipBinary: true }
+        ))
+        .pipe(replace(
+            /(<\/body>)/,
+            `\t${fs.readFileSync('counter.html', 'utf8')}$1`, { skipBinary: true }
+        ));
 
-	return merge(files, html)
-		.pipe(gulp.dest('dest'));
+    return merge(files, html)
+        .pipe(gulp.dest('dest'));
 });
 
 gulp.task('sync', () => {
-	return gulp.src('dest/**')
-		.pipe(rsync({
-			root: 'dest',
-			hostname: 'shwr.me',
-			destination: '/var/www/shwr.me/html',
-			recursive: true,
-			clean: true,
-			incremental: true,
-			silent: true
-		}));
+    return gulp.src('dest/**')
+        .pipe(rsync({
+            root: 'dest',
+            hostname: 'shwr.me',
+            destination: '/var/www/shwr.me/html',
+            recursive: true,
+            clean: true,
+            incremental: true,
+            silent: true
+        }));
 });
 
 gulp.task('deploy', gulp.series(
-	'clean',
-	'build',
-	'assets',
-	'sync'
+    'clean',
+    'build',
+    'assets',
+    'sync'
 ));
