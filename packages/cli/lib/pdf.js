d300cb44b18f8f758e1fc4aa165fec3edd508f49
Nikolay Govorov <nikolay_govorov@bk.ru>
2018-08-21 23:55:27 +0300

    Added the dynamic loading page size, fixes #6

--- packages/cli/lib/pdf.js
+++ packages/cli/lib/pdf.js
@@ -1,5 +1,10 @@
 const puppeteer = require('puppeteer')
 
+function evalCalcExpression (expression) {
+  // eslint-disable-next-line no-eval
+  return eval(expression.replace(/calc/g, '').replace(/px/g, '')) + 'px'
+}
+
 function pdf ({ root }, { file }) {
   let browser, page
 
@@ -13,7 +18,20 @@ function pdf ({ root }, { file }) {
       page = p
     })
     .then(() => page.goto(`file://${root}/index.html`))
-    .then(() => page.pdf({ path: file, width: '960px', height: '600px' }))
+    .then(() => page.evaluate(async () => {
+      const container = document.querySelector('.shower')
+
+      const styles = window.getComputedStyle(container)
+
+      return [
+        evalCalcExpression(styles.getPropertyValue('--slide-width')),
+        evalCalcExpression(styles.getPropertyValue('--slide-height'))
+      ]
+    }))
+    .then(([width, height]) => page.pdf({ path: file, width, height }))
+    .catch(error => {
+      console.error(error)
+    })
     .then(() => {
       browser.close()
     })
