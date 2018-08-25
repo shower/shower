36c39eb850cc47ba92d8f1fae816665085aa76d8
Nikolay Govorov <nikolay_govorov@bk.ru>
2018-08-25 04:57:23 +0300

    Implemented the ability to customize the presentation when creating

--- packages/cli/source/command/create.js
+++ packages/cli/source/command/create.js
@@ -1,41 +1,60 @@
 const fs = require('fs')
 const path = require('path')
 const vfs = require('vinyl-fs')
+const inquirer = require('inquirer')
 const { promisify } = require('util')
 const template = require('gulp-template')
 
 const { installDependencies } = require('../util/npm')
 
-function create ({ root }, { directory = 'slides' }) {
+async function create ({ root }, { directory = 'slides' }) {
   const options = {
-    name: 'slides',
-    ratio: '16 / 9',
-    theme: 'ribbon',
     template: 'presentation',
     year: (new Date()).getFullYear()
   }
+
+  process.stdout.write('\n')
+
+  const params = await inquirer
+    .prompt([{
+      name: 'name',
+      type: 'input',
+      default: 'slides',
+      message: 'Input presentation name'
+    }, {
+      name: 'theme',
+      type: 'list',
+      message: 'Select theme',
+      choices: ['ribbon', 'material']
+    }, {
+      name: 'ratio',
+      type: 'input',
+      default: '16 / 9',
+      message: 'Select presentation ratio'
+    }])
+
+  Object.assign(options, params)
 
   if (path.isAbsolute(directory)) {
     directory = path.join(root, directory)
   }
 
-  return Promise.resolve()
-    // 1. Create new folder
-    .then(() => promisify(fs.mkdir)(directory))
+  // 1. Create new folder
+  await promisify(fs.mkdir)(directory)
 
-    // 2. Create 'index.html'and 'package.json'
-    .then(() => new Promise((resolve, reject) => {
-      vfs.src(['**/**'], {
-        cwd: path.join(__dirname, '..', '..', 'templates', options.template)
-      })
-        .pipe(template(options))
-        .pipe(vfs.dest(directory))
-        .on('end', resolve)
-        .on('error', reject)
-    }))
+  // 2. Create 'index.html'and 'package.json'
+  await new Promise((resolve, reject) => {
+    vfs.src(['**/**'], {
+      cwd: path.join(__dirname, '..', '..', 'templates', options.template)
+    })
+      .pipe(template(options))
+      .pipe(vfs.dest(directory))
+      .on('end', resolve)
+      .on('error', reject)
+  })
 
-    // 3. Install dependencies
-    .then(() => installDependencies(directory, ['shower-core', `shower-${options.theme}`]))
+  // 3. Install dependencies
+  await installDependencies(directory, ['shower-core', `shower-${options.theme}`])
 }
 
 create.messages = {
