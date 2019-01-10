c4c76ebbcf8cd6ceff289b417c9ab624f0d314d8
Aleksey Shvayka <shvaikalesh@gmail.com>
2019-01-10 16:21:59 +0200

    Integrate `sauce-connect-launcher` (#138)
    * Integrate `sauce-connect-launcher`
    
    * Turn off Travis CI emails
    
    * Check for `sauceProcess`
    
    * Do not try to launch `sauce-connect` in Travis

--- packages/core/test/func-harness.js
+++ packages/core/test/func-harness.js
@@ -3,6 +3,10 @@
 const { EventEmitter } = require('events');
 const { Server } = require('http');
 const handler = require('serve-handler');
+const childProcess = require('child_process');
+const sauceConnect = require('sauce-connect-launcher');
+
+EventEmitter.defaultMaxListeners = 0;
 
 const port = Number(process.env.npm_package_config_port);
 const server = new Server((request, response) => {
@@ -12,12 +16,43 @@ const server = new Server((request, response) => {
     });
 });
 
+let sauceProcess;
+
 module.exports = {
-    before: done => {
-        EventEmitter.defaultMaxListeners = 0;
-        server.listen(port, done);
+    asyncHookTimeout: 60000,
+    isSauce: true,
+
+    'chrome-local': {
+        isSauce: false,
     },
-    after: done => {
-        server.close(done);
+
+    before(done) {
+        childProcess.execSync('npm run build');
+
+        server.listen(port, () => {
+            if (this.isSauce && !process.env.TRAVIS) {
+                console.log('Starting Sauce Connect proxy...');
+                sauceConnect((err, res) => {
+                    if (err) {
+                        console.error(err.message);
+                    } else {
+                        sauceProcess = res;
+                        done();
+                    }
+                });
+            } else {
+                done();
+            }
+        });
+    },
+
+    after(done) {
+        server.close(() => {
+            if (sauceProcess) {
+                sauceProcess.close(done);
+            } else {
+                done();
+            }
+        });
     },
 };
