c34e095b3d08eb9996fe0defd7be91cabe45306e
Aleksey Shvayka <shvaikalesh@gmail.com>
2019-06-20 12:19:06 +0300

    Add failing tests for extra history entries

--- packages/core/test/func/location.js
+++ packages/core/test/func/location.js
@@ -1,5 +1,25 @@
 'use strict';
 
+const getHistoryLength = browser => {
+    return new Promise(resolve => {
+        browser.execute(
+            function() {
+                return history.length;
+            },
+            function({ value }) {
+                resolve(value);
+            },
+        );
+    });
+};
+
+const getHistoryDelta = async (browser, path) => {
+    const before = await getHistoryLength(browser);
+    browser.url(`${browser.launchUrl}${path}`);
+    const after = await getHistoryLength(browser);
+    return after - before;
+};
+
 module.exports = {
     '@tags': ['location'],
 
@@ -198,6 +218,30 @@ module.exports = {
         browser.url(`${browser.launchUrl}/full#3`);
         browser.refresh();
         browser.assert.urlContains('#3');
+        browser.end();
+    },
+
+    'no extra history entries: `list` mode': async browser => {
+        const delta = await getHistoryDelta(browser, '/list');
+        browser.assert.equal(delta, 1);
+        browser.end();
+    },
+
+    'no extra history entries: `list` mode and slide hash': async browser => {
+        const delta = await getHistoryDelta(browser, '/list-id#id');
+        browser.assert.equal(delta, 1);
+        browser.end();
+    },
+
+    'no extra history entries: `full` query': async browser => {
+        const delta = await getHistoryDelta(browser, '/list?full');
+        browser.assert.equal(delta, 1);
+        browser.end();
+    },
+
+    'no extra history entries: `full` query and slide hash': async browser => {
+        const delta = await getHistoryDelta(browser, '/list?full#1');
+        browser.assert.equal(delta, 1);
         browser.end();
     },
 };
