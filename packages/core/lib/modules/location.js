c67fba39b4440c27c03396f758b590dc1ca9e6c9
Aleksey Shvayka <shvaikalesh@gmail.com>
2019-06-22 10:22:30 +0300

    Prevent extra history entries being pushed on init

--- packages/core/lib/modules/location.js
+++ packages/core/lib/modules/location.js
@@ -1,7 +1,7 @@
 import { freezeHistory } from '../utils';
 
 export default shower => {
-    const getURL = () => {
+    const composeURL = () => {
         const search = shower.isFullMode ? '?full' : '';
         const slide = shower.activeSlide;
         const hash = slide ? `#${slide.id}` : '';
@@ -9,43 +9,45 @@ export default shower => {
         return location.pathname + search + hash; // path is required to clear search params
     };
 
-    const changeSlide = () => {
-        const id = location.hash.slice(1);
-        if (!id) return;
-
-        const target = shower.slides.find(slide => slide.id === id);
-        if (target) {
-            target.activate();
-        } else if (!shower.activeSlide) {
-            shower.first(); // invalid hash
-        }
-    };
-
-    shower.addEventListener('modechange', () => {
-        history.replaceState(null, document.title, getURL());
-    });
-
-    shower.addEventListener('slidechange', () => {
-        history.pushState(null, document.title, getURL());
-    });
-
-    shower.addEventListener('start', () => {
+    const applyURLMode = () => {
         const isFull = new URL(location).searchParams.has('full');
-        changeSlide();
-        if (isFull) {
-            shower.enterFullMode();
-        }
-    });
 
-    window.addEventListener('popstate', () => {
         freezeHistory(() => {
-            changeSlide();
-            const isFull = new URL(location).searchParams.has('full');
             if (isFull) {
                 shower.enterFullMode();
             } else {
                 shower.exitFullMode();
             }
         });
+    };
+
+    const applyURLSlide = () => {
+        const id = location.hash.slice(1);
+        if (!id) return;
+
+        const target = shower.slides.find(slide => slide.id === id);
+        if (target) {
+            freezeHistory(() => {
+                target.activate();
+            });
+        } else if (!shower.activeSlide) {
+            shower.first(); // invalid hash
+        }
+    };
+
+    const applyURL = () => {
+        applyURLMode();
+        applyURLSlide();
+    };
+
+    shower.addEventListener('modechange', () => {
+        history.replaceState(null, document.title, composeURL());
     });
+
+    shower.addEventListener('slidechange', () => {
+        history.pushState(null, document.title, composeURL());
+    });
+
+    shower.addEventListener('start', applyURL);
+    window.addEventListener('popstate', applyURL);
 };
