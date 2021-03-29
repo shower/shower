eae785dda30a2ce7349b8cd579836f57f15e2641
Igor Korovchenko <igsekor@gmail.com>
2021-03-29 21:31:19 +0300

    Add new options for command 'shower create' (#58)

--- packages/cli/core/command/create.js
+++ packages/cli/core/command/create.js
@@ -10,24 +10,101 @@ const template = require('gulp-template')
 
 const { installDependencies } = require('../lib/npm')
 
-async function handler ({ cwd, directory: folderName = 'slides', yes: isDefault }) {
+const cmdArgs = [
+  {
+    name: 'yes',
+    alias: ['y'],
+    default: false,
+    type: 'boolean'
+  },
+  {
+    name: 'force',
+    default: false,
+    type: 'boolean'
+  },
+  {
+    name: 'no-interactive',
+    alias: ['s'],
+    default: false,
+    type: 'boolean'
+  },
+  {
+    name: 'theme',
+    alias: ['t'],
+    default: 'ribbon',
+    type: 'list',
+    choices: ['ribbon', 'material'],
+    interactive: true
+  },
+  {
+    name: 'ratio',
+    alias: ['r'],
+    default: '16:9',
+    type: 'list',
+    choices: ['16:9', '4:3'],
+    interactive: true
+  }
+]
+
+function prepareOptions (args) {
+  const opts = {}
+  for (const index in args) {
+    const n = args[index].name
+    opts[n] = args[index]
+    delete opts[n].name
+    if ('interactive' in opts[n]) {
+      delete opts[n].interactive
+    }
+  }
+  return opts
+}
+
+function prepareParams (args) {
+  const pars = []
+  for (const index in args) {
+    if ('interactive' in args[index]) {
+      if ('interactive' in args[index]) {
+        delete args[index].interactive
+      }
+      if ('choices' in args[index]) {
+        args[index].type = 'list'
+      }
+      pars.push(args[index])
+    }
+  }
+  return pars
+}
+
+async function handler ({
+  cwd,
+  directory: folderName = 'slides',
+  yes: isDefault,
+  force: isOverride = false,
+  'no-interactive': isNonInteractive = false,
+  theme: themeName = 'ribbon',
+  ratio: aspectRatio = '16:9'
+}) {
   // Let's check if such folder exists
   const directory = path.isAbsolute(folderName) ? folderName : path.join(cwd, folderName)
 
   if (fs.existsSync(directory)) {
-    const { isForce } = await inquirer.prompt({
-      name: 'isForce',
-      type: 'confirm',
-      default: false,
-      message: `The ${chalk.yellow(folderName)} dir already exists. Do you want to overwrite it?`
-    })
+    if (!isOverride) {
+      const { isForce } = await inquirer.prompt({
+        name: 'isForce',
+        type: 'confirm',
+        default: false,
+        message: `The ${chalk.yellow(folderName)} dir already exists. Do you want to overwrite it?`
+      })
 
-    if (isForce) {
-      await del([directory])
+      if (isForce) {
+        await del([directory])
+      } else {
+        process.stdout.write(chalk.red('\n Creating aborted\n'))
+
+        return
+      }
     } else {
-      process.stdout.write(chalk.red(`\n Creating aborted\n`))
-
-      return
+      await del([directory])
     }
   }
 
@@ -41,22 +118,21 @@ async function handler ({ cwd, directory: folderName = 'slides', yes: isDefault 
     ratio: '16:9'
   }
 
-  const params = [{
-    name: 'theme',
-    type: 'list',
-    message: 'Select theme',
-    choices: ['ribbon', 'material']
-  }, {
-    name: 'ratio',
-    type: 'list',
-    message: 'Select presentation ratio',
-    choices: ['16:9', '4:3']
-  }]
+  const params = prepareParams(cmdArgs)
 
   if (isDefault) {
     Object.assign(options, defaultParams)
+  } else if (isNonInteractive) {
+    Object.assign(options, {
+      theme: themeName,
+      ratio: aspectRatio
+    })
   } else {
     Object.assign(options, await inquirer.prompt(params))
+  }
+
+  if (params[0].choices.includes(themeName) && params[1].choices.includes(aspectRatio)) {
+    process.stdout.write(chalk.red('\n Creating aborted. Choose valid values.\n'))
   }
 
   options.ratio = options.ratio.replace(/:/, ' / ')
@@ -99,13 +175,7 @@ async function handler ({ cwd, directory: folderName = 'slides', yes: isDefault 
 
 function builder (yargs) {
   return yargs
-    .options({
-      yes: {
-        alias: ['y'],
-        default: false,
-        type: 'boolean'
-      }
-    })
+    .options(prepareOptions(cmdArgs))
     .positional('directory', {
       default: 'slides',
       type: 'string'
