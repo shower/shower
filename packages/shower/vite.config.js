f9f01fe48eba23e5f605d4d5a11c9b14c0cc8b4c
Vadim Makeev <hi@pepelsbey.dev>
2026-03-17 15:58:28 +0100

    Add shower dev server, name consistently

---
+++ packages/shower/vite.config.js
@@ -0,0 +1,24 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	server: {
+		fs: {
+			allow: ['..'],
+		},
+	},
+	plugins: [
+		{
+			name: 'serve-shower-packages',
+			configureServer(server) {
+				server.middlewares.use((req, res, next) => {
+					if (req.url?.startsWith('/node_modules/@shower/')) {
+						const rest = req.url.slice('/node_modules/@shower/'.length);
+						req.url = '/@fs/' + resolve(import.meta.dirname, '..', rest);
+					}
+					next();
+				});
+			},
+		},
+	],
+});
